0385e4aa81703d01614dfffe43527ffcd7d9a252
tijscluyts <r0983582@student.thomasmore.be>
2024-08-09 16:04:19 +0200

    ticket

--- queries/movie_queries.py
+++ queries/movie_queries.py
@@ -1,2 +1,3 @@
 movie_total_tickets_query = "select count(ticketId) as total from movie.tickets"
 movie_review_query = "select stars, naam, bericht from movie.review where stars = %s;"
+insert_review_query = "insert into movie.review (stars, naam, bericht) values (%s, %s, %s);"
